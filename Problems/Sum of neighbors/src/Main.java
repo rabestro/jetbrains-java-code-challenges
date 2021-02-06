44a0edcff11199f5eb9b3d4fe6c1d44c4ed1db20
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-06 11:52:43 +0200

    Add read matrix from input

--- Problems/Sum of neighbors/src/Main.java
+++ Problems/Sum of neighbors/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import static java.util.function.Predicate.not;
+
 class Main {
+    private static final Pattern SPACE = Pattern.compile("\\s+");
+    private static final Pattern NEW_LINE = Pattern.compile("\\R");
+
     public static void main(String[] args) {
-        // put your code here
+        final var matrix = new Scanner(System.in)
+                .useDelimiter(NEW_LINE).tokens()
+                .takeWhile(not("end"::equals))
+                .map(s -> SPACE.splitAsStream(s).mapToInt(Integer::parseInt).toArray())
+                .toArray(int[][]::new);
+
+        final var rows = matrix.length;
+        final var cols = matrix[0].length;
+
     }
 }
